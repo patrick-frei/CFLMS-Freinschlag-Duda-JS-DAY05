291526cb08051456acf02575fcf5acc89ef185ab
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-08 13:39:53 +0200

    adds js

--- Challenge/script.js
+++ Challenge/script.js
@@ -1,6 +1,12 @@
+let carElements = document.getElementsByClassName("cars")
 $.getJSON("data.json", function(cars) {
-    
     for (i in cars){
-        console.log(cars[i].name)}
-    
+		carElements[i].children[0].innerHTML = cars[i].name
+		carElements[i].children[1].innerHTML += `<p>Production Year: ${cars[i].productionYear}</p>`
+		carElements[i].children[1].innerHTML += `<p>Price: ${cars[i].price}</p>`
+		carElements[i].children[1].innerHTML += `<img src=${cars[i].img}>`
+		carElements[i].children[0].addEventListener("click", function(element) {
+			element.target.nextElementSibling.classList.toggle("insert")
+		})
+	}
 })
