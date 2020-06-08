777d10c5a54702846c09fd16c79626edc46f75dc
Dominik Duda <dominik.duda6@gmail.com>
2020-06-08 15:22:23 +0200

    angepasst

--- Basic/Exercise1/js/script.js
+++ Basic/Exercise1/js/script.js
@@ -1,7 +1,14 @@
-let sandwiches = `{ "sandwich": "hamburger", "calories": "260" }`
-let fries = `{ "fries_size": "Large French Fries", "calories": "570" }`
+let sandwiches = `{ "sandwich": "hamburger", "calories": "260", "ShouldIEatIt": false }`
+let fries = `{ "fries_size": "Large French Fries", "calories": "570", "ShouldIEatIt": true }`
 
 let sandwichesObj = JSON.parse(sandwiches)
 let friesObj = JSON.parse(fries)
+
+console.log(sandwichesObj)
+console.log(friesObj)
+console.table(sandwichesObj)
+
+console.table(friesObj)
+
 
 document.write(`My favorite sandwich is a ${sandwichesObj.sandwich} which has approximately ${sandwichesObj.calories} calories, along with it I enjoy eating ${friesObj.fries_size} which have about ${friesObj.calories} calories`)
